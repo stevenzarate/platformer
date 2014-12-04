a73d576645c800f9462ad5256670aa4047470a66
steven <stevengzarate68@gmail.com>
2014-12-04 14:00:10 -0800

    no message

--- core/src/com/zarates/platformer/model/Player.java
+++ core/src/com/zarates/platformer/model/Player.java
@@ -18,10 +18,10 @@ public class Player {
 
         TextureRegion[][] spriteSheetFrames = TextureRegion.split(spriteSheet, 70, 100) ;//it splits up our spritsheet to width and height and stores it in a two dimensional array
 
-        int counter = 0;// counts the sprites we have in our texture
+        int counter = 0;
         for(int row = 0; row < spriteSheetFrames.length;row++){//selecting rows that we're on
             for (int column = 0; column < spriteSheetFrames[row].length; column++){//selecting columns for the player
-                counter++;//adds one to the thing
+                counter++;// counts the sprites we have in our texture
             }
         }
 
